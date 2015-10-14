a2964170efb719d6db7ce9a9d725bb44d13b5497
Ido Ran <ido.ran@gmail.com>
2015-10-14 22:00:08 +0300

    Build very simple UI

--- app/index.js
+++ app/index.js
@@ -1,13 +1,65 @@
 var accessToken = localStorage.getItem("accessToken");
 
-var xhr = new XMLHttpRequest();
+var dataSource;
+var currMediaIndex = -1;
+var imgElement;
+var lastUpdateAt;
+var imgContainerElement;
+var loadingContainerElement;
 
-var url = '/photos';
-xhr.open('GET', url);
-xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-xhr.onload = function(e) {
-  console.log('result', this.response);
+window.onload = function() {
+  imgContainerElement = document.getElementById("imgcontainer");
+  imgElement = document.getElementById('mainimg');
+  loadingContainerElement = document.getElementById('loadingcontainer');
+  loadMedia();
 }
 
-console.log('requesting data');
-xhr.send();
+function loadMedia() {
+  var xhr = new XMLHttpRequest();
+
+  var url = '/photos';
+  xhr.open('GET', url);
+  xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+  xhr.onload = function(e) {
+    if (this.status === 401) {
+      // unauthorized
+      location = 'https://ran-smart-frame.appspot.com/authorize';
+      return;
+    }
+
+    lastUpdateAt = new Date();
+    dataSource = JSON.parse(this.response);
+
+    loadingContainerElement.style.display = 'none';
+    imgContainerElement.style.display = 'block';
+
+    showNextPhoto();
+  }
+
+  xhr.send();
+}
+
+function showNextPhoto() {
+  if (checkMediaFreshness()) return;
+
+  currMediaIndex++;
+  if (currMediaIndex > dataSource.Media.length - 1) {
+    currMediaIndex = 0;
+  }
+
+  imgElement.src = dataSource.Media[currMediaIndex].URL;
+  setTimeout(showNextPhoto, 5000);
+}
+
+function checkMediaFreshness() {
+  var now = new Date();
+  var diffMs = (now - lastUpdateAt);
+  var diffMin = diffMs / 1000 / 60;
+
+  if (diffMin > 1) {
+    loadMedia();
+    return true;
+  }
+
+  return false;
+}
